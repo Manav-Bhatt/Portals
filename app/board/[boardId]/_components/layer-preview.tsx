2aa6d89faa7c002dd31fd2885cbb33cf7bc9d473
Manav <manavbhatt056@gmail.com>
2025-09-03 06:10:49 +0530

    added text and sticky note component

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -5,6 +5,8 @@ import { LayerType } from "@/types/canvas";
 import { memo } from "react";
 import { Rectangle } from "./rectangle";
 import { Ellipse } from "./ellipse";
+import { Text } from "./text";
+import { Note } from "./note";
 
 interface LayerPreviewProps {
     id: string;
@@ -18,6 +20,56 @@ export const LayerPreview = memo(
         if (!layer) return null;
 
         switch (layer.type) {
+            case LayerType.Note:
+
+
+            return (
+
+
+                <Note
+
+
+                    id={id}
+
+
+                    layer={layer}
+
+
+                    onPointerDown={onLayerPointerDown}
+
+
+                    selectionColor={selectionColor}
+
+
+                />
+
+
+            );
+            case LayerType.Text:
+
+
+            return (
+
+
+                <Text
+
+
+                    id={id}
+
+
+                    layer={layer}
+
+
+                    onPointerDown={onLayerPointerDown}
+
+
+                    selectionColor={selectionColor}
+
+
+                />
+
+
+            );
             case LayerType.Ellipse:
 
 
